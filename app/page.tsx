fcce0e776f89d98d8504c02bc1d9e485a6bed517
Alex <gameprodakshen@gmail.com>
2025-07-21 18:36:16 +0300

    SSR error test 4

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,12 @@
 'use client';
-import Navbar from '@/components/ui/Navbar';
-
-import Hero from '@/components/home/HeroSection';
 
 import CanvasCoinWrapper from '@/components/ui/Safe3DWrapper';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground p-6 md:p-8">
-      <Navbar />
-      <Hero />
+      {/* <Navbar />
+      <Hero /> */}
 
       <CanvasCoinWrapper />
 {/* 
