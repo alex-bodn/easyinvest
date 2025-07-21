acaac7b0223674e6cee95ef65735fbfdfc6d3910
Alex <gameprodakshen@gmail.com>
2025-07-21 18:46:02 +0300

    Hero section test 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 'use client';
+
+import dynamic from 'next/dynamic';
+
 import Navbar from '@/components/ui/Navbar';
 
 import OutAdvantaves from '@/components/home/OurAdvantagesSection';
@@ -8,11 +11,15 @@ import CallToActionSection from '@/components/home/CallToActionSection';
 
 import CanvasCoinWrapper from '@/components/ui/Safe3DWrapper';
 
+const Hero = dynamic(() => import('@/components/home/HeroSection'), {
+  ssr: false,
+});
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground p-6 md:p-8">
       <Navbar />
-
+      <Hero />
 
       <CanvasCoinWrapper />
 
